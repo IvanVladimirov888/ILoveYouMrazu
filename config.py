fa1221a3e053d35423731868ec1b77725f7085b2
Ivan <ivan_vladimirov_888@mail.ru>
2025-06-08 19:35:39 +0300

    new ivan

--- config.py
+++ config.py
@@ -12,7 +12,7 @@ DB_CONFIG = {
     'host': os.getenv('DB_HOST') or 'localhost',
     'database': os.getenv('DB_NAME') or 'Alik',
     'user': os.getenv('DB_USER') or 'root',
-    'password': os.getenv('DB_PASSWORD') or 'Elinarina824+',
+    'password': os.getenv('DB_PASSWORD') or 'Ivan_Vladimirov_888',
     'port': os.getenv('DB_PORT') or 3306,
     'auth_plugin': 'mysql_native_password'
 }
